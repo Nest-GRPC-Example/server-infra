8585391bc0d3a268a69e2502f840b07d85212f60
Ko-GyeongTae <kokt0203@naver.com>
2022-06-05 05:25:05 +0900

    Chore: docker run script 수정

--- pullDocker.sh
+++ pullDocker.sh
@@ -14,9 +14,9 @@ docker pull kokt0203/nest-order-svc
 
 echo Start Containers...
 
-docker run -d kokt0203/nest-api-gateway --env-file ../api-gateway/.env -p 3000:3000 --name nest-api-gateway
-docker run -d kokt0203/nest-auth-svc --env-file ../auth-server/.env -p 50051:50051 --name nest-auth-svc
-docker run -d kokt0203/nest-order-svc --env-file ../order-server/.env -p 50052:50052 --name nest-order-svc
-docker run -d kokt0203/nest-product-svc --env-file ../product-server/.env -p 50053:50053 --name nest-product-svc
+docker run -d --env-file ../api-gateway/.env -p 3000:3000 --name nest-api-gateway kokt0203/nest-api-gateway /bin/bash
+docker run -d --env-file ../auth-server/.env -p 50051:50051 --name nest-auth-svc kokt0203/nest-auth-svc /bin/bash
+docker run -d --env-file ../order-server/.env -p 50052:50052 --name nest-order-svc kokt0203/nest-order-svc /bin/bash
+docker run -d --env-file ../product-server/.env -p 50053:50053 --name nest-product-svc kokt0203/nest-product-svc /bin/bash
 
 echo DONE!
