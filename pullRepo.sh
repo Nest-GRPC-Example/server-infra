d8c4184e45c8c301118de9a23cfa2fdb166a7805
Ko-GyeongTae <kokt0203@naver.com>
2022-06-05 16:21:03 +0900

    Feat: pullRepo.sh 추가

---
+++ pullRepo.sh
@@ -0,0 +1,23 @@
+cd ../
+echo Start to pull repo : api-gateway
+git pull origin main
+cd api-gateway
+yarn
+
+cd ../
+echo Start to pull repo : auth-server
+git pull origin main
+cd api-gateway
+yarn
+
+cd ../
+echo Start to pull repo : product-server
+git pull origin main
+cd api-gateway
+yarn
+
+cd ../
+echo Start to pull repo : order-server
+git pull origin main
+cd api-gateway
+yarn
